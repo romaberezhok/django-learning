3db5f43739e5c330afdf42512d0652c43babd9a5
romaberezhok <rberezhok@gmail.com>
2020-04-13 16:36:09 +0300

    fix login

--- SocialNetwork/bookmarks/account/urls.py
+++ SocialNetwork/bookmarks/account/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from . import views
 
-app_name = 'account'
+# app_name = 'account'
 
 urlpatterns = [
     # previous login view
     # path('login/', views.user_login, name='login'),
-    re_path('^login/$', auth_views.LoginView.as_view(), name="login"),
-    re_path('^logout/$', auth_views.LogoutView.as_view(), name="logout"),
-    path('', views.dashboard, name='dashboard')
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
