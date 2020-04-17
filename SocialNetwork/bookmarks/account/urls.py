44e092f36578919dbd9bb1d792cfa35ed9dfd3b0
romaberezhok <rberezhok@gmail.com>
2020-04-17 22:48:46 +0300

    add register

--- SocialNetwork/bookmarks/account/urls.py
+++ SocialNetwork/bookmarks/account/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     # previous login view
     # path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
